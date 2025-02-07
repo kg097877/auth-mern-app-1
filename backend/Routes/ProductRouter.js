6b9bfc2d7cf134ce80f1bc5951d4eed98e2d77e6
kg097877 <kg097877@gmail.com>
2025-02-08 01:27:04 +0530

    Update ProductRouter.js

--- backend/Routes/ProductRouter.js
+++ backend/Routes/ProductRouter.js
@@ -1,19 +1,18 @@
 const ensureAuthenticated = require('../Middlewares/Auth');
-
 const router = require('express').Router();
+const path = require('path'); //Import the path module
+
 
 router.get('/', ensureAuthenticated, (req, res) => {
-    console.log('---- logged in user detail ---', req.user);
-    res.status(200).json([
-        {
-            name: "mobile",
-            price: 10000
-        },
-        {
-            name: "tv",
-            price: 20000
-        }
-    ])
+    // console.log('--- logged in user detail ---', req.user);
+    const filePath = path.join(__dirname, 'frontend/public/homepage.html'); // Replace with your file path
+
+    try {
+        res.sendFile(filePath); //Send the file
+    } catch (error) {
+        console.error('Error sending file:', error);
+        res.status(500).send('Error serving file'); //Handle errors appropriately
+    }
 });
 
 module.exports = router;
